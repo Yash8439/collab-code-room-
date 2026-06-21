edeb41031fb977391d127273a2759592d1e35d60
Yash Rastogi <rastogiy66@gmail.com>
2026-06-21 14:55:33 +0530

    feat: add Docker support - multi-stage build with Nginx

--- client/src/api/execute.js
+++ client/src/api/execute.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 export const executeCode = async (code, language, stdin = '') => {
   const res = await axios.post(`${BASE_URL}/api/execute`, {
@@ -10,6 +10,7 @@ export const executeCode = async (code, language, stdin = '') => {
   });
   return res.data;
 };
+
 export const reviewCode = async (code, language) => {
   const res = await axios.post(`${BASE_URL}/api/review`, { code, language });
   return res.data;
